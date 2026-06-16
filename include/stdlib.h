91a31c3a138a3b31531aa2fcba2963ebb4a83f5d
Metroynome <yort124@gmail.com>
2026-06-16 15:15:56 -0400

    updates

--- include/stdlib.h
+++ include/stdlib.h
@@ -13,7 +13,7 @@
 
 
 //--------------------------------------------------------
-__LIBRAC1_INLINE__ void* malloc( unsigned long size );
-__LIBRAC1_INLINE__ void free( void* ptr );
+// __LIBRAC1_INLINE__ void* malloc( unsigned long size );
+// __LIBRAC1_INLINE__ void free( void* ptr );
 
 #endif // _LIBRAC1_STDLIB_H_
--- include/string.h
+++ include/string.h
@@ -7,8 +7,8 @@
 __LIBRAC1_INLINE__ void * memset(void * ptr, int value, int num);
 __LIBRAC1_INLINE__ void * memcpy(void * destination, const void * source, int num);
 __LIBRAC1_INLINE__ char * strncpy(char *dest, const char *src, int n);
-__LIBRAC1_INLINE__ int strcmp(char *str1, char *str2);
-__LIBRAC1_INLINE__ int strncmp(char * str1, char * str2, int n);
-__LIBRAC1_INLINE__ int strlen(const char *str);
+// __LIBRAC1_INLINE__ int strcmp(char *str1, char *str2);
+// __LIBRAC1_INLINE__ int strncmp(char * str1, char * str2, int n);
+// __LIBRAC1_INLINE__ int strlen(const char *str);
 
 #endif // _LIBRAC1_STRING_H_
